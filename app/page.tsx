5c50e353a057c797d3250ea3f03c2873a13a2b06
Ji Park <ji@jipark.dev>
2022-11-10 11:53:49 +1100

    Update to my-post.

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Link href={{ pathname: "[id]", query: { id: "Hello World" } }}>
+      <Link href={{ pathname: "/blog/[slug]", query: { slug: "my-post" } }}>
         Hyperlink
       </Link>
     </div>
